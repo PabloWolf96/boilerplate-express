8933402e74ab28449fad1e387ace165cdd466d6e
Pablowolf96 <ikhena.owen47@gmail.com>
2022-07-07 16:23:26 +0100

    add route handler for /now

--- myApp.js
+++ myApp.js
@@ -16,5 +16,15 @@ app.get("/json", (req, res) => {
   }
   res.json({ message: response });
 });
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
 
 module.exports = app;
