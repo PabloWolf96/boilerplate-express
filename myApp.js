30efeb7c946e564e2bfd1476af612db8524cf865
Pablowolf96 <ikhena.owen47@gmail.com>
2022-07-07 15:34:24 +0100

    configure envs

--- myApp.js
+++ myApp.js
@@ -1,11 +1,15 @@
 let express = require("express");
 let app = express();
+require("dotenv").config();
 app.use("/public", express.static(__dirname + "/public"));
 console.log("Hello World");
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    return res.json({ message: "HELLO WORLD" });
+  }
   res.json({ message: "Hello json" });
 });
 
