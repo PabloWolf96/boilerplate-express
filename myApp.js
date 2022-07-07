38b7217726f1930e2e6f84971cf1c3ab936f062c
Pablowolf96 <ikhena.owen47@gmail.com>
2022-07-07 15:28:15 +0100

    create route handler for /json

--- myApp.js
+++ myApp.js
@@ -5,5 +5,8 @@ console.log("Hello World");
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+app.get("/json", (req, res) => {
+  res.json({ message: "Hello json" });
+});
 
 module.exports = app;
